2aa803f9c9ca33599b39a04bd16252ec81ac17a3
Winka Wino Yunanda <winkawinoy@gmail.com>
2023-11-01 17:25:47 +0700

    Warm Up and Adding New

--- Logic/Day04/Day04/MiniMaxSum.cs
+++ Logic/Day04/Day04/MiniMaxSum.cs
@@ -6,6 +6,20 @@
         {
             Console.WriteLine("=====Min Max Sum====");
             Console.WriteLine("Masukkan nilai Array:");
+            int[] deret = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+            Array.Sort(deret);
+            int min = 0;
+            int max = 0;    
+            for (int i = 0; i < deret.Length; i++)
+            {
+                if (i < deret.Length - 1)
+                  min += deret[i];
+                
+                if (i > 0)
+                  max += deret[i];
+            }
+            Console.WriteLine($"{min} {max}");
         }
     }
 }
