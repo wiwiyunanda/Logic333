f949daa656081b36f6087d83e27a64b37baa5045
Winka Wino Yunanda <winkawinoy@gmail.com>
2023-11-06 16:45:15 +0700

    recursive

--- Logic/Day06/RecursiveDigitSum.cs
+++ Logic/Day06/RecursiveDigitSum.cs
@@ -6,6 +6,28 @@ namespace Day06
         public RecursiveDigitSum()
         {
             Console.WriteLine("=====Recursive Digit Sum=====");
+            Console.Write("Masukkan input: ");
+            string input = Console.ReadLine();
+            Console.Write("Masukkan perulangan: ");
+            int ulang = int.Parse(Console.ReadLine());
+
+            int result = Super_Digit(String.Concat(Enumerable.Repeat(input, ulang)));
+            Console.WriteLine(result);
+
+        }
+
+        private int Super_Digit(string str)
+        {
+            int result = 0;
+               foreach (var item in str)
+                {
+                    result += int.Parse(item.ToString());
+                }
+               if (result.ToString().Length >1)
+                {
+                    return Super_Digit(result.ToString());
+                }
+            return result;
         }
     }
 }
